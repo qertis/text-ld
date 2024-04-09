3b8960b7d821fdf0312e9a52638e272807cbe59e
Deni Baskovsky <qertis@users.noreply.github.com>
2024-04-09 20:20:51 +0300

    eyo-kernel fixes

--- test/index.cjs
+++ test/index.cjs
@@ -1,9 +1,9 @@
 const test = require('node:test');
 const assert = require('node:assert/strict');
-const textLd = require('../dist/index.js').default;
+const textLD = require('../dist/index.cjs').default;
 
 test('Text LD', async () => {
-    const context = await textLd(' Какой-то #интересный текс ');
+    const context = await textLD(' Какой-то #интересный текс ');
     assert.deepEqual(context.encodingFormat, 'plain/text');
     assert.deepEqual(context.inLanguage, 'ru');
     assert.ok(context.keywords.includes('#интересный'));
